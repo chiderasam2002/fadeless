c1d473331acad40843ee62db73016d20c97381a9
chiderasam2002 <Samuelchidera67@gmail.com>
2024-09-17 15:40:27 +0100

    newsletter

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-  const button = document.getElementById('submit')
+
+const button = document.getElementById('submit')
+const myName   = document.getElementById('name')
+const email  = document.getElementById('email')
 
 
-  button.addEventListener("click", SendAlart)
+
+
+form.addEventListener('submit', (e) => {
+  
+
+  if(myName === '' || email === ''){
+    e.preventDefault()
+  }
+
+ 
+
+
+   button.addEventListener("click", SendAlart)
 
 
 
    function SendAlart(){
-    alert("Subscribed successfully")
- }
+      alert("Subscribed successfully")
+  }
+
+
+})
 
 
