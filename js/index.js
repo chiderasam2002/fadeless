80b15d78dd76778cdd9aa5d22cb961ba72466a5c
chiderasam2002 <165075524+chiderasam2002@users.noreply.github.com>
2025-01-27 12:08:38 +0100

    media querry correction

--- js/index.js
+++ js/index.js
@@ -12,18 +12,3 @@
 
 
 
-// function subscribe()  {
-  
-
-//     const buttonElement = document.querySelector('.submit')
-   
-
-
-//     if(buttonElement.innertext === 'subscribe') {
-//         buttonElement.innerHTML = 'subscribed';
-//     }
-//     else{
-//         buttonElement.innerHTML = 'subscribe';
-//     }
-
-// }
