367ad5f5f41c7a6a5319c304fe9128f24bdcf037
chiderasam2002 <165075524+chiderasam2002@users.noreply.github.com>
2025-02-14 20:17:00 +0100

    error update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
+const name = document.getElementById("name")
+const email = document.getElementById("email")
+const  form = document.getElementById("form")
+const erroElemen = document.getElementById("error")
 
 
 
 
 
+form.addEventListener('submit', (e) => {
+
+let message = []
+
+if (name.value === '' || name.value == null){
+   
+}
 
 
+if (message.length > 0){
+    e.preventDefault()
+    
+}
 
 
-
-
-
-
-
+   
+})
