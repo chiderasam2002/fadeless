079a1a33256add36fd01bdfe4bde2b8eb5d3997b
chiderasam2002 <165075524+chiderasam2002@users.noreply.github.com>
2025-02-17 13:33:24 +0100

    form update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,64 @@
-const name = document.getElementById("name")
-const email = document.getElementById("email")
-const  form = document.getElementById("form")
-const erroElemen = document.getElementById("error")
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
 
 
 
+form.addEventListener('submit',  e =>{
+    e.preventDefault();
+    validateInputs();
+});
 
 
-form.addEventListener('submit', (e) => {
+const setError = (element, message) => {
+    const inputcontrol = element.parentElement;
+    const errorDisplay = inputcontrol.querySelector('error');
 
-let message = []
+    errorDisplay.innerText = message;
+    inputcontrol.classList.add('error');
+    inputcontrol.classList.remove('success');
+}
 
-if (name.value === '' || name.value == null){
-   
+const setSuccess = element => {
+    const inputcontrol = element.parentElement;
+    const errorDisplay = inputcontrol.querySelector('error');
+
+    errorDisplay.innerText = '';
+    inputcontrol.classList.add('success');
+    inputcontrol.classList.remove('error');
+
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+
+    const name = name.value.trim(); 
+    const email = email.value.trim(); 
+
+if(name === '' || name === null){
+    setError(name, 'username is required');
+}else{
+    setSuccess(name);
 }
 
 
-if (message.length > 0){
-    e.preventDefault()
-    
+
+
+if(email === '' || email === null){
+    setError(name, 'email is required');
+}else if (!isValidEmail(emailValue)){
+    setError(email, 'provide a valid email address');
+}else{
+    setSuccess(email);
 }
 
 
-   
-})
+
+};
+
+
+
